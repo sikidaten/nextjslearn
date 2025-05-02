66fe51f1894ffee74e708d450011d967e8060601
Naoki Wada <sikidaten@gmail.com>
2025-05-02 12:00:22 +0900

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import '@/app/ui/global.css';
+import  {imbplexmono} from '@/app/ui/fonts';
+ 
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${imbplexmono.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import {IBM_Plex_Mono,Lusitana} from 'next/font/google';
+
+export const imbplexmono = IBM_Plex_Mono({weight: ['400']});
+export const lusitana = Lusitana({weight: ['400','700']});
