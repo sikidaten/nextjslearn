7f0064972653c855593dc880980fb09ba4a6c798
Naoki Wada <sikidaten@gmail.com>
2025-05-02 13:15:42 +0900

    fix font error

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
 import {IBM_Plex_Mono,Lusitana} from 'next/font/google';
 
-export const imbplexmono = IBM_Plex_Mono({weight: ['400']});
-export const lusitana = Lusitana({weight: ['400','700']});
+export const imbplexmono = IBM_Plex_Mono({weight: ['400'],subsets:['latin']});
+export const lusitana = Lusitana({weight: ['400','700'],subsets:['latin']});
